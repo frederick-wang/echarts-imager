fc0e200a726a3a9ab903692563dde5ff7219cae4
Zhaoji Wang <hwoam@outlook.com>
2022-05-31 14:21:12 +0800

    feat: add --buffer output option

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
-const { stdin, argv, exit } = require('process')
-const { readFileSync } = require('fs')
+const { stdin, argv, exit } = require('node:process')
+const { readFileSync } = require('node:fs')
 const { Command } = require('commander')
 const { error } = require('./log')
 const { version, name } = require('../package.json')
+
 const program = new Command()
 program
   .name(name)
@@ -27,40 +28,20 @@ program
   )
   .option('-w, --width, [width]', 'the width of output chart file', '1024')
   .option('-h, --height, [height]', 'the height of output chart file', '768')
-
-/**
- *
- * @param { { input?: string, output?: string, format: string, width: string, height: string } } opts
- * @param {string[]} args
- *
- * @returns { { input?: string, output?: string, format: string, width: string, height: string } }
- */
-function parse(opts, args) {
-  if (!args.length) {
-    return opts
-  } else if (args.length === 1) {
-    if (opts.input) {
-      opts.output = opts.output || args[0]
-    } else {
-      opts.input = opts.input || args[0]
-    }
-  } else {
-    opts.input = opts.input || args[0]
-    opts.output = opts.output || args[1]
-  }
-  return opts
-}
+  .option(
+    '-b, --buffer',
+    'output the JSON representation of buffer of the chart file (will not output the chart file)'
+  )
 
 /**
  * Get options from command line
  *
- * @returns {Promise<{ input?: string, output?: string, format: string, width: string, height: string }>}
+ * @returns {Promise<{ input?: string, output?: string, format: string, width: string, height: string, buffer: boolean }>}
  */
 const __getOptions = () =>
   new Promise((resolve, reject) => {
     try {
       let input = ''
-
       if (stdin.isTTY) {
         program.parse(argv)
         resolve(parse(program.opts(), program.args))
@@ -93,11 +74,11 @@ const __getOptions = () =>
 /**
  * Get options from command line
  *
- * @returns {Promise<{ option: any; output: string; format: string; width: number; height: number }>}
+ * @returns {Promise<{ option: any; output: string; format: string; width: number; height: number, buffer: boolean }>}
  */
 async function getOptions() {
   const options = await __getOptions()
-  const { input, output } = options
+  const { input, output, buffer } = options
   let { format, width, height } = options
   width = Number(width.replace(/[a-zA-Z]/g, ''))
   height = Number(height.replace(/[a-zA-Z]/g, ''))
@@ -116,17 +97,40 @@ async function getOptions() {
     if (typeof option !== 'object') {
       throw new Error('Echart Option is not a JSON object')
     }
-    return { option, output, format, width, height }
+    return { option, output, format, width, height, buffer }
   } catch (_) {
     try {
       const option = JSON.parse(readFileSync(input).toString())
-      return { option, output, format, width, height }
+      return { option, output, format, width, height, buffer }
     } catch (e) {
       error(e.message)
       error('Invalid Echart Option')
       exit(1)
     }
   }
+}
+
+/**
+ *
+ * @param { { input?: string, output?: string, format: string, width: string, height: string, buffer: boolean } } opts
+ * @param {string[]} args
+ *
+ * @returns { { input?: string, output?: string, format: string, width: string, height: string, buffer: boolean } }
+ */
+function parse(opts, args) {
+  if (!args.length) {
+    return opts
+  } else if (args.length === 1) {
+    if (opts.input) {
+      opts.output = opts.output || args[0]
+    } else {
+      opts.input = opts.input || args[0]
+    }
+  } else {
+    opts.input = opts.input || args[0]
+    opts.output = opts.output || args[1]
+  }
+  return opts
 }
 
 module.exports.getOptions = getOptions
--- src/generate.js
+++ src/generate.js
@@ -1,37 +1,85 @@
-const echarts = require('echarts')
-const { exit } = require('process')
+const { exit } = require('node:process')
 const { writeFileSync } = require('node:fs')
-const { getOptions } = require('./cli')
-const { info, error } = require('./log')
+const { Buffer } = require('node:buffer')
+
+const echarts = require('echarts')
 const sharp = require('sharp')
 const { JSDOM } = require('jsdom')
 const { createCanvas } = require('canvas')
+
+const { getOptions } = require('./cli')
+const { info, error } = require('./log')
+const {
+  supportedFormats,
+  supportedTransparentFormats
+} = require('./supported-formats.json')
+
+// Initialize the DOM for Echarts
+const { window } = new JSDOM()
+global.window = window
+global.navigator = window.navigator
+global.document = window.document
 echarts.setPlatformAPI({ createCanvas })
 
-const supportedFormats = [
-  'heic',
-  'heif',
-  'avif',
-  'jpeg',
-  'jpg',
-  'png',
-  'raw',
-  'tiff',
-  'tif',
-  'webp',
-  'gif',
-  'jp2',
-  'jpx',
-  'j2k',
-  'j2c'
-]
-const supportedTransparentFormats = ['png', 'webp', 'gif']
 async function generate() {
-  const { option, format, output, width, height } = await getOptions()
-  const { window } = new JSDOM()
-  global.window = window
-  global.navigator = window.navigator
-  global.document = window.document
+  const { option, format, output, width, height, buffer } = await getOptions()
+  const root = createRootNode(width, height)
+  const initOptions = {
+    renderer: 'svg',
+    width: `${width}px`,
+    height: `${height}px`
+  }
+  const chart = echarts.init(root, null, initOptions)
+  chart.setOption({ ...option, animation: false })
+  const svgString = chart.renderToSVGString()
+
+  if (buffer) {
+    if (format === 'svg') {
+      console.log(JSON.stringify(Buffer.from(svgString)))
+    } else if (supportedFormats.includes(format)) {
+      const sharpObj = sharp(Buffer.from(svgString)).toFormat(format)
+      const buf = await (supportedTransparentFormats.includes(format)
+        ? sharpObj
+        : sharpObj.flatten({ background: { r: 255, g: 255, b: 255 } })
+      ).toBuffer()
+      console.log(JSON.stringify(buf))
+    } else {
+      error(`Unsupported format: ${format}`)
+    }
+    exit(0)
+  }
+
+  if (output) {
+    if (format === 'svg') {
+      writeFileSync(output, svgString)
+      info(`The chart has been saved to ${output}`)
+    } else if (supportedFormats.includes(format)) {
+      const sharpObj = sharp(Buffer.from(svgString)).toFormat(format)
+      const buf = await (supportedTransparentFormats.includes(format)
+        ? sharpObj
+        : sharpObj.flatten({ background: { r: 255, g: 255, b: 255 } })
+      ).toBuffer()
+      writeFileSync(output, buf)
+      info(`The chart has been saved to ${output}`)
+    } else {
+      error(`Unsupported format: ${format}`)
+    }
+    exit(0)
+  }
+
+  console.log(svgString)
+  exit(0)
+}
+
+/**
+ * Create the root element for Echarts
+ *
+ * @param {number} width the width of the chart
+ * @param {number} height the height of the chart
+ *
+ * @returns the HTML Element of the chart for Echarts
+ */
+function createRootNode(width, height) {
   const root = document.createElement('div')
   root.style.cssText = `width: ${width}px; height: ${height}px;`
   Object.defineProperty(root, 'clientWidth', {
@@ -40,37 +88,7 @@ async function generate() {
   Object.defineProperty(root, 'clientHeight', {
     value: height
   })
-  const initOptions = {
-    renderer: 'svg',
-    // ssr: true,
-    width: `${width}px`,
-    height: `${height}px`
-  }
-  const chart = echarts.init(root, null, initOptions)
-  chart.setOption({ ...option, animation: false })
-  const svgString = chart.renderToSVGString()
-
-  if (output) {
-    if (format === 'svg') {
-      writeFileSync(output, svgString)
-      info(`The chart has been saved to ${output}`)
-    } else if (supportedFormats.includes(format)) {
-      const sharpObj = sharp(Buffer.from(svgString)).toFormat(format)
-      writeFileSync(
-        output,
-        await (supportedTransparentFormats.includes(format)
-          ? sharpObj
-          : sharpObj.flatten({ background: { r: 255, g: 255, b: 255 } })
-        ).toBuffer()
-      )
-      info(`The chart has been saved to ${output}`)
-    } else {
-      error(`Unsupported format: ${format}`)
-    }
-  } else {
-    console.log(svgString)
-  }
-  exit(0)
+  return root
 }
 
 module.exports.generate = generate
